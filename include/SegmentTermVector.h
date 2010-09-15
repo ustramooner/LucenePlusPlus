6f172d462bbe0984b2ad58db3ffbd769a7158676
Ben van Klinken <ustramooner@users.sourceforge.net>
2010-09-15 17:34:14 +0200

    fixed incorrect inheritance

--- include/SegmentTermVector.h
+++ include/SegmentTermVector.h
@@ -10,7 +10,7 @@
 
 namespace Lucene
 {
-	class LPPAPI SegmentTermVector : public TermPositionVector, public LuceneObject
+	class LPPAPI SegmentTermVector : public TermFreqVector, public LuceneObject
 	{
 	public:
 		SegmentTermVector(const String& field, Collection<String> terms, Collection<int32_t> termFreqs);
