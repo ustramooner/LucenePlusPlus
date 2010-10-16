49e352c0f9fd441cfbb46e73cff5ae9d561632d6
alanw <alanwright.atex@googlemail.com>
2010-10-16 14:37:11 +0100

    Fixed compile issues on 64-bit platforms
    closes #6

--- src/core/include/_NearSpansUnordered.h
+++ src/core/include/_NearSpansUnordered.h
@@ -14,7 +14,7 @@ namespace Lucene
 		LUCENE_CLASS(SpansCell);
 	
 	protected:
-		NearSpansUnorderedWeakPtr unordered;
+		NearSpansUnorderedWeakPtr _unordered;
 		SpansPtr spans;
 		SpansCellPtr _next;
 		int32_t length;
