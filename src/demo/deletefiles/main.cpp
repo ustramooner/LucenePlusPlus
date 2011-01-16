b405bf5bd901ba995a1c85edd45eb29dff5b66bf
Ben van Klinken <ustramooner@users.sourceforge.net>
2011-01-16 10:07:19 +1000

    Update file headers with new year

--- src/demo/deletefiles/main.cpp
+++ src/demo/deletefiles/main.cpp
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////
 #include "LuceneHeaders.h"
-// Copyright (c) 2009-2010 Alan Wright. All rights reserved.
+// Copyright (c) 2009-2011 Alan Wright. All rights reserved.
 // Distributable under the terms of either the Apache License (Version 2.0)
 // or the GNU Lesser General Public License.
 /////////////////////////////////////////////////////////////////////////////
